3a5a1c17f1768cebffad8a355818516d8016dc96
RobertHernandezArenas <roberthernandezarenas@gmail.com>
2025-02-25 14:36:51 +0100

    :wrench: modify errors

--- app/src/main/java/com/campusdual_grupo3/bookandgo/utils/animations/Animations.kt
+++ app/src/main/java/com/campusdual_grupo3/bookandgo/utils/animations/Animations.kt
@@ -4,7 +4,6 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.util.TypedValue
 import android.view.View
-import // android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 
 object Animations {
