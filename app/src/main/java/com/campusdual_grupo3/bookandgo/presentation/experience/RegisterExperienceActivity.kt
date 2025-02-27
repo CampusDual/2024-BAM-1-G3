5b64a2ebbe52e66725671121b4ced51fb3f58ee0
ana131repo <antakarana131@gmail.com>
2025-02-27 12:58:14 +0100

    Add layout_register_experience

---
+++ app/src/main/java/com/campusdual_grupo3/bookandgo/presentation/experience/RegisterExperienceActivity.kt
@@ -0,0 +1,21 @@
+package com.campusdual_grupo3.bookandgo.presentation.experience
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.campusdual_grupo3.bookandgo.R
+
+class RegisterExperienceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_experience)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
